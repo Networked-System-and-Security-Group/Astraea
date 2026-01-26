f6a8ecda5b2c8c0c7b517fabf1336afc88132a3f
pengqy <udeyxx@gmail.com>
2026-01-26 04:15:43 +0000

    set mDataLen in doca_buf_set_data_len

--- src/broker/Buf.h
+++ src/broker/Buf.h
@@ -1,19 +1,16 @@
 #ifndef ASTRAEA_BUF_H_
 #define ASTRAEA_BUF_H_
 
-#include <cstddef>
-#include <cstdint>
-#include <doca_error.h>
-
 #include <doca_buf.h>
 #include <doca_buf_inventory.h>
+#include <doca_error.h>
 #include <doca_mmap.h>
 
-#include "common.h"
+#include <cstddef>
 
 namespace astraea {
 class Buf {
-  public:
+   public:
     doca_buf *mBuf = nullptr;
     doca_buf_inventory *mInv = nullptr;
     doca_mmap *mMmap = nullptr;
@@ -21,5 +18,5 @@ class Buf {
     size_t mBufLen = 0;
     size_t mDataLen = 0;
 };
-} // namespace astraea
+}  // namespace astraea
 #endif
