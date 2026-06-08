c734c52f6bf7ca38757ad7a1579ff40d04c9a6b3
pengqy <pengqy@buaa.edu.cn>
2026-06-08 07:48:40 +0000

    find better parameter

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -11,9 +11,9 @@ alias mh=./build/memscan_host
 alias lh=./build/localec_host
 
 
-alias ac="taskset -c 1-3 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=150 ./build/cdn_dpu -r 50000"
-alias ar="taskset -c 4-6 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=8000 ./build/replica_dpu"
-alias al="taskset -c 7-9 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=50 DMA_SLA=50 ./build/localec_dpu"
+alias ac="taskset -c 1-3 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=300 ./build/cdn_dpu -r 50000"
+alias ar="taskset -c 4-6 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=4000 ./build/replica_dpu"
+alias al="taskset -c 7-9 env LD_PRELOAD=./build/libastraea_broker.so EC_SLA=200 DMA_SLA=50 ./build/localec_dpu"
 alias am="taskset -c 10-12 env LD_PRELOAD=./build/libastraea_broker.so DMA_SLA=80000 ./build/memscan_dpu"
 
 
